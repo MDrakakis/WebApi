105fa59f1eba40b5858edf3a857d5ad646a3205f
mdrakakis <mike.drak24@gmail.com>
2020-07-30 13:40:10 +0300

    UserController Changes

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -5,16 +5,23 @@ using WebApi.Services;
 
 namespace WebApi.Controllers
 {
+  [Produces("application/json")]
   [Route("api/[controller]")]
   [ApiController]
   public class UsersController : ControllerBase
   {
+    #region Service
+
     private readonly UserService _userService;
 
     public UsersController(UserService userService)
     {
       _userService = userService;
     }
+
+    #endregion Service
+
+    #region Get
 
     [HttpGet(Name = "Get")]
     public ActionResult<List<User>> Get() =>
@@ -26,12 +33,14 @@ namespace WebApi.Controllers
       var user = _userService.Get(id);
 
       if (user == null)
-      {
         return NotFound();
-      }
 
       return user;
     }
+
+    #endregion Get
+
+    #region Post
 
     [HttpPost]
     public ActionResult<User> Create(User user)
@@ -40,20 +49,26 @@ namespace WebApi.Controllers
       return CreatedAtRoute(nameof(Get), new { id = user.Id.ToString() }, user);
     }
 
+    #endregion Post
+
+    #region Put
+
     [HttpPut("{id:length(24)}")]
     public IActionResult Update(string id, User userIn)
     {
       var user = _userService.Get(id);
 
       if (user == null)
-      {
         return NotFound();
-      }
 
       _userService.Update(id, userIn);
 
       return RedirectToAction();
     }
+
+    #endregion Put
+
+    #region Delete
 
     [HttpDelete("{id:length(24)}")]
     public IActionResult Remove(string id)
@@ -61,13 +76,13 @@ namespace WebApi.Controllers
       var user = _userService.Get(id);
 
       if (user == null)
-      {
         return NotFound();
-      }
 
       _userService.Remove(user.Id);
 
       return RedirectToAction();
     }
+
+    #endregion Delete
   }
 }
